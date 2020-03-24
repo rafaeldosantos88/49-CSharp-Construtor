dfd45db7fb4cb2b9f442d762f92b9f5f27020e06
Rafael Dos Santos <raphael_979@hotmail.com>
2020-03-24 17:52:08 -0400

    Uso de construtor

---
+++ Construtor/Produto.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+namespace Construtor
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome,double preco,int quantidade)//Construtor geralmente é depois dos atributos
+        {//Não faz sentido um nome nulo,preço valendo zero,quantidade 0?Então pra evitar um produto sem nome,sem preço,quantidade
+         //Então obrigamos a iniciação com valor criando um construtor,ai o programador vai ser obrigado  informa valores na hora de instanciar produtos.
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Construtor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Construtor: é uma operação especial da classe,que executa no momento da instanciação do objeto.
+             * Uso Comuns:
+             -Iniciar valor dos atributos.
+             -permitir ou obriar que o objeto receba dados / dependências no momento de sua instanciação(injeção de depedência)
+             •Se um construtor customizado não for especificado,a classe disponibiliza o construtor padrão.
+             -Produto p= new Produto();
+             •É possivel especificar mais de um construtor na mesma classe(Sobrecarga).*/
+
+            
+            
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Produto p = new Produto(nome, preco, quantidade);//Aqui instanciei o Produto
+
+
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p); 
+        }
+    }
+}
+
+
+
